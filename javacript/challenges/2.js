4cb3ec4da8ef332124d2f11f8e68f95fe3bab60e
onyeka mmakwe <notmaks@yahoo.com>
2020-03-13 22:29:35 +0100

    update typo

--- javacript/challenges/2.js
+++ javacript/challenges/2.js
@@ -3,7 +3,7 @@
 // is to create a feature that askes the user for their age based on their age tell
 // them if they are allowed to watch it. Piece of cake right!!!
 
-// using prompt() and alert(), ask a user for their age.
+// using prompt()  ask a user for their age.
 // IF they say they are below 18, respond with:
 // "Sorry, you are too young to view this movie. Try our 'E' Catergory"
 
